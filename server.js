abe8aad2ae06f9b95eaea572cc974f572e3e0b6a
mehrdadkhv <alikhodaverdi682gmail.com>
2022-02-23 16:00:05 +0330

    simpleRoute

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const http =require('http');
 const products = require('./data/products.json');
 
+
+
 const server = http.createServer((req,res)=>{
-    if(req.url==='/api/products'){
+    if(req.url==='/api/products' && req.method ==='GET'){
         res.writeHead(200,{'Content-Type': 'application/json'});
         res.end(JSON.stringify(products));
+    }else{
+        res.writeHead(404,{'Content-Type': 'application/json'});
+        res.end(JSON.stringify({message: "route not found"}));
     }
-
 }) 
 
 
